5b7103af4fc1671c8c9a6a722f73389d0f1a2c85
GeonilJang <jgi0105@naver.com>
2018-08-28 23:22:50 +0900

    설정파일을 이용하여 객체를 생성해서 주입사용

---
+++ src/main/java/com/test/di/TestMain.java
@@ -0,0 +1,21 @@
+package com.test.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		String confLocation = "classpath:test.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(confLocation);
+		TestDAO testDao = ctx.getBean("TestDAOImp", TestDAOImp.class);
+		
+		System.out.println("===============================");
+		testDao.printMsg();
+		System.out.println("===============================");
+	}
+
+}
