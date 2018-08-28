97858c85755342719c8b3208d162e90f5bb83b11
GeonilJang <jgi0105@naver.com>
2018-08-29 01:22:12 +0900

    프로퍼티 방식을 이용한 자세한 설명

---
+++ src/main/java/com/test/diEx04/MyBatisDao.java
@@ -0,0 +1,16 @@
+package com.test.diEx04;
+
+public class MyBatisDao {
+	
+	public MyBatisDao() {}//기본생성자
+	
+	public void insertDB() {
+		System.out.println("insert logic..");
+	}
+	
+	public void updateDB() {
+		System.out.println("update logic..");
+	}
+	
+		
+}
